4e20a43e6372633886506356faa7eee0c6a303c1
Praisecodes <cmancoder@gmail.com>
2023-12-28 18:26:22 +0100

    [update] display navigators where necessary

--- navigators/main_app_navigators.tsx
+++ navigators/main_app_navigators.tsx
@@ -1,10 +1,11 @@
 import { useLoggedInState } from "../zustand/AppStore";
 import AuthNavigators from "./auth_navigators";
+import MainScreensNavigators from "./main_screens_navigators";
 
 const MainAppNavigators = (): React.ReactNode => {
   const loggedIn = useLoggedInState((state) => state.loggedIn);
 
-  return (loggedIn ? (<></>) :
+  return (loggedIn ? (<MainScreensNavigators />) :
     <AuthNavigators />
   )
 }
