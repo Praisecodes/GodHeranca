a6aa88b7b5bb97281e121d2e9eddec4e63bd2f1c
Praisecodes <cmancoder@gmail.com>
2023-12-30 23:57:59 +0100

    [fix] fix animations

--- screens/account_setup/select_identity_document.tsx
+++ screens/account_setup/select_identity_document.tsx
@@ -6,6 +6,7 @@ import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-na
 
 const SelectIdentityDocument = ({ navigation }: { navigation: any; }): React.ReactNode => {
   const initialBoarder = useSharedValue<number>(0);
+  const initialScale = useSharedValue<number>(0);
   const [selected, setSelected] = useState<string>("");
   const [documentTypes, setDocumentTypes] = useState<any[]>([
     {
@@ -39,23 +40,36 @@ const SelectIdentityDocument = ({ navigation }: { navigation: any; }): React.Rea
               <TouchableWithoutFeedback
                 key={index}
                 onPress={() => {
+                  initialBoarder.value = withTiming(0, { duration: 0 });
+                  initialScale.value = withTiming(0.5, { duration: 0 });
                   setSelected(documentType.name);
+                  initialBoarder.value = withTiming(2, { duration: 50 });
+                  initialScale.value = withTiming(1, { duration: 50 });
                 }}
               >
                 <Animated.View style={[
                   tw`flex flex-row items-center rounded-lg py-1 px-4 bg-[#F1F1F1]`,
-                  {
-                    borderWidth: documentType.name == selected ? 2 : 0,
+                  useAnimatedStyle(() => ({
                     borderColor: "black",
-                  }
+                    borderWidth: documentType.name == selected ? initialBoarder.value : 0,
+                  }), [selected])
                 ]}
                 >
                   <Text style={[tw`flex-1 text-black text-lg py-4`, { fontFamily: "satoshi-bold" }]}>
                     {documentType.title}
                   </Text>
 
-                  <View style={[tw`w-[1.7rem] p-[1.9px] h-[1.7rem] border-2 rounded-full`]}>
-                    {documentType.name == selected && (<View style={[tw`bg-black rounded-full w-[100%] h-[100%]`]} />)}
+                  <View style={[tw`w-[1.7rem] p-1 h-[1.7rem] border-2 rounded-full`]}>
+                    <Animated.View
+                      style={[
+                        tw`bg-black rounded-full w-[100%] h-[100%]`,
+                        useAnimatedStyle(() => ({
+                          transform: [
+                            { scale: documentType.name == selected ? initialScale.value : 0 }
+                          ]
+                        }), [selected])
+                      ]}
+                    />
                   </View>
                 </Animated.View>
               </TouchableWithoutFeedback>
