cf30cf91bab2b90da43e759e02409661af0c1024
Praisecodes <cmancoder@gmail.com>
2023-12-30 13:23:09 +0100

    [update] add dropdown for guarantor relationship

--- screens/account_setup/guarantor.tsx
+++ screens/account_setup/guarantor.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AccountSetupLayout from '../../layouts/account_setup_layout';
 import { Text, TouchableWithoutFeedback, View } from 'react-native';
 import tw from "twrnc";
 import { SetupInput } from '../../components/atoms';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 const Guarantor = ({ navigation }: { navigation: any }): React.ReactNode => {
+  const [dropDownOpen, setDropDownOpen] = useState<boolean>(false);
+  const [relationship, setRelationship] = useState<any>(null);
+  const [options, setOptions] = useState<any[]>([
+    { label: "Father", value: "father" },
+    { label: "Mother", value: "mother" },
+    { label: "Brother", value: "brother" },
+    { label: "Sister", value: "sister" },
+    { label: "Friend", value: "friend" },
+    { label: "Collegue", value: "collegue" },
+  ]);
   return (
     <AccountSetupLayout navigation={navigation}>
       <View style={[tw`flex flex-col gap-y-6 justify-between h-[100%]`]}>
-        <View style={[tw`flex flex-col gap-y-9 mt-5`]}>
+        <View style={[tw`flex flex-col gap-y-10 mt-5`]}>
           <View style={[tw`flex flex-col gap-y-2`]}>
             <Text style={[tw`text-xl px-1`, { fontFamily: "satoshi-bold" }]}>
               Guarantor Name
@@ -27,24 +38,22 @@ const Guarantor = ({ navigation }: { navigation: any }): React.ReactNode => {
             />
           </View>
 
-          {/* <View style={[tw`flex flex-col gap-y-2`]}>
-            <Text style={[tw`text-xl px-1`, { fontFamily: "satoshi-bold" }]}>
-              Address
-            </Text>
-            <SetupInput
-              placeholder='Stret Address'
-            />
-          </View>
-
           <View style={[tw`flex flex-col gap-y-2`]}>
             <Text style={[tw`text-xl px-1`, { fontFamily: "satoshi-bold" }]}>
-              Postal Code
+              Who is the Guarantor to you?
             </Text>
-            <SetupInput
-              placeholder='000000'
-              keyboardType="number-pad"
+            <DropDownPicker
+              open={dropDownOpen}
+              value={relationship}
+              setOpen={setDropDownOpen}
+              setValue={setRelationship}
+              items={options}
+              setItems={setOptions}
+              placeholder="Guarantor Relationship"
+              style={[tw`border-0 bg-[#F6F6F6] text-lg py-4 px-5`]}
+              textStyle={[tw`text-lg`, { fontFamily: "satoshi" }]}
             />
-          </View> */}
+          </View>
         </View>
 
         <TouchableWithoutFeedback>
