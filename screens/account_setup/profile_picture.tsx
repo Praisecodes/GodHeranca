85092b2c241e0edbbec7ff324e364eff8c79a278
Praisecodes <cmancoder@gmail.com>
2024-01-01 01:54:15 +0100

    [update] work on profile picture page

--- screens/account_setup/profile_picture.tsx
+++ screens/account_setup/profile_picture.tsx
@@ -5,10 +5,13 @@ import tw from "twrnc";
 import { FontAwesome5 } from '@expo/vector-icons';
 import { MediaTypeOptions, launchImageLibraryAsync } from 'expo-image-picker';
 import { SuccessModal } from '../../components/templates/modals';
+import { useAccountSetupState } from '../../zustand/AccountSetupStore';
 
 const ProfilePicture = ({ navigation }: { navigation: any; }): React.ReactNode => {
   const [image, setImage] = useState<string | null>(null);
   const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const setup_info = useAccountSetupState((state) => state.setup_info);
+  const updateSetupInfo = useAccountSetupState((state) => state.updateSetupInfo);
 
   const toggleModal = () => setModalOpen(!modalOpen);
 
@@ -21,7 +24,11 @@ const ProfilePicture = ({ navigation }: { navigation: any; }): React.ReactNode =
       return;
     }
 
-    setImage(result.assets[0].uri);
+    // setImage(result.assets[0].uri);
+    updateSetupInfo({
+      ...setup_info,
+      profilePicture: result.assets[0].uri
+    });
   }
 
   return (
@@ -40,37 +47,45 @@ const ProfilePicture = ({ navigation }: { navigation: any; }): React.ReactNode =
               Upload a profile picture
             </Text>
 
-            {image == null && (<TouchableWithoutFeedback onPress={() => { getImageFromDevice() }}>
+            {setup_info.profilePicture == "" && (
               <View style={[tw`mx-auto rounded-100 p-16 bg-[#F3F3F3]`]}>
                 <FontAwesome5 name="user-alt" size={64} color="#A7A7A7" />
               </View>
-            </TouchableWithoutFeedback>)}
-
-            {image !== null && (
-              <TouchableWithoutFeedback onPress={getImageFromDevice}>
-                <Image source={{ uri: image }} width={300} height={300} style={[tw`rounded-full mx-auto w-[13rem] h-[13rem]`]} />
-              </TouchableWithoutFeedback>
             )}
 
-            {image !== null && (
+            {setup_info.profilePicture !== "" && (
+              <Image source={{ uri: setup_info.profilePicture }} width={300} height={300} style={[tw`rounded-full mx-auto w-[13rem] h-[13rem]`]} />
+            )}
+
+            {setup_info.profilePicture !== "" && (
               <Text style={[tw`text-base text-center`, { fontFamily: "satoshi" }]}>
-                Click on the image to select new image
+                Click on the "Change" button to select new image
               </Text>
             )}
           </View>
 
-          <View style={[tw`flex flex-row justify-between gap-x-4 items-center`]}>
-            <TouchableWithoutFeedback onPress={() => { navigation.navigate('address') }}>
-              <Text style={[tw`text-black flex-1 text-center bg-[#E6E6E6] py-3 text-lg rounded-full`, { fontFamily: "satoshi-bold" }]}>
-                Skip
-              </Text>
-            </TouchableWithoutFeedback>
+          <View>
+            <View style={[tw`flex flex-row justify-between gap-x-4 items-center`]}>
+              {setup_info.profilePicture == "" && (<TouchableWithoutFeedback onPress={() => { navigation.navigate('address') }}>
+                <Text style={[tw`text-black flex-1 text-center bg-[#E6E6E6] py-3 text-lg rounded-full`, { fontFamily: "satoshi-bold" }]}>
+                  Skip
+                </Text>
+              </TouchableWithoutFeedback>)}
 
-            <TouchableWithoutFeedback onPress={toggleModal}>
-              <Text style={[tw`text-white flex-1 text-center bg-black py-3 text-lg rounded-full`, { fontFamily: "satoshi-bold" }]}>
-                Continue
-              </Text>
-            </TouchableWithoutFeedback>
+              <TouchableWithoutFeedback onPress={getImageFromDevice}>
+                <Text style={[tw`text-white flex-1 text-center bg-black py-3 text-lg rounded-full`, { fontFamily: "satoshi-bold" }]}>
+                  {setup_info.profilePicture == "" ? "Choose" : "Change"}
+                </Text>
+              </TouchableWithoutFeedback>
+            </View>
+
+            {setup_info.profilePicture !== "" && (
+              <TouchableWithoutFeedback onPress={() => { navigation.navigate('address') }}>
+                <Text style={[tw`text-white text-center mt-4 bg-black py-3 text-lg rounded-full`, { fontFamily: "satoshi-bold" }]}>
+                  Save And Continue
+                </Text>
+              </TouchableWithoutFeedback>
+            )}
           </View>
         </View>
       </AccountSetupLayout>
