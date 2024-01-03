b194d395be283db4d3f06e3c93492bca7d94556e
Praisecodes <cmancoder@gmail.com>
2024-01-03 15:17:52 +0100

    [update] adjust layout and all

--- layouts/app_screen_layout.tsx
+++ layouts/app_screen_layout.tsx
@@ -4,7 +4,7 @@ import tw from 'twrnc';
 
 const AppScreenLayout = ({ children }: { children: React.ReactNode; }) => {
   return (
-    <View style={[tw`flex-1 pt-6 px-3`]}>
+    <View style={[tw`flex-1 pt-6 px-4`]}>
       <ScrollView contentContainerStyle={[tw`min-h-[100%] pb-6`]}>
         {children}
       </ScrollView>
--- screens/app/home.tsx
+++ screens/app/home.tsx
@@ -1,16 +1,33 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableWithoutFeedback } from 'react-native'
 import React, { useEffect } from 'react'
 import { AppScreenLayout } from '../../layouts';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { appRootStackParamList } from '../../navigators/types';
+import tw from 'twrnc';
+import { Ionicons } from '@expo/vector-icons';
 
 const Home = ({ navigation }: { navigation: NativeStackNavigationProp<appRootStackParamList, "Home"> }): React.ReactNode => {
 
   return (
     <AppScreenLayout>
-      <Text>
-        Home
-      </Text>
+      <View style={[tw`flex flex-row justify-between py-3 items-center`]}>
+        <View style={[tw`flex flex-row items-center gap-x-2`]}>
+          <View style={[tw`p-8 rounded-full bg-[#aaaaaa]`]}></View>
+
+          <View>
+            <Text style={[tw`text-base`, { fontFamily: "satoshi" }]}>
+              Good Afternoon
+            </Text>
+            <Text numberOfLines={1} style={[tw`text-xl`, { fontFamily: "satoshi-bold" }]}>
+              Martinez White
+            </Text>
+          </View>
+        </View>
+
+        <TouchableWithoutFeedback>
+          <Ionicons name="ios-notifications-outline" size={30} color="black" />
+        </TouchableWithoutFeedback>
+      </View>
     </AppScreenLayout>
   )
 }
